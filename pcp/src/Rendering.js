ca45ffb68291bd08e76f3a6fd7fdb4977034c247
Pierre <pierre.wiele@student.htw-berlin.de>
2024-05-14 15:34:16 +0200

    Cleaning Code Note: parts of the code still exists solely for a better understanding, and visualisation of functions, since javascript is a new concept to me.

--- pcp/src/Rendering.js
+++ pcp/src/Rendering.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import ReactDOM from 'react-dom/client';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { PCDLoader } from 'three/addons/loaders/PCDLoader.js';
@@ -6,45 +7,22 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { randFloat, randInt } from 'three/src/math/MathUtils';
 import AddScene from './AddScene';
 
-
-
-
 function RenderOnCanvas(event) {
-
-    const canvas = THREE.createCanvasElement();
+    const canvas = event.target //THREE.createCanvasElement();
     const renderer = new THREE.WebGLRenderer({ antialias: true, canvas, alpha: true });
     renderer.setClearColor(0x000000, 0);
-    //const fov = 60; //fov - field of view
-    //const aspect = 1; // the canvas default
-    //const near = 0.1;
-    //const far = 1000;
-    //const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    //camera.position.set(0, 50, 0);
-    //camera.up.set(0, 3, 1);
-    //camera.lookAt(0, 3, 1);
-
-    const sceneInfo = AddScene(event.target);
+    canvas.nextElementSibling.classList.add("hidden");
+    const sceneInfo = AddScene(canvas.parentNode);
     const scene = sceneInfo.scene;
     const camera = sceneInfo.camera;
-
     scene.rotateX(-0.4);
-    //scene.rotateY(0.2);
-    //loader.load('/beethoven_2.xyz', function (points){});
-    //loader.load()
 
     {
-
         const color = 0xFFFFFF;
         const intensity = 600;
         const light = new THREE.PointLight(color, intensity);
-        //light.position.x = 10;
-        //light.position.y = 1;
-        //light.position.z = 10;
-        //light.raycast();
         scene.add(light);
-
     }
-
 
 // an array of objects who's rotation to update
     const objects = [];
@@ -94,8 +72,6 @@ function RenderOnCanvas(event) {
     });
 
     const { left, right, top, bottom, width, height } = sceneInfo.displayElement.getBoundingClientRect();
-    //scene.fog = new THREE.Fog(0x000000, 40, 75);
-    //scene.background = new THREE.Color(0xaaaaff);
     camera.fov = 100000 / width;
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
@@ -136,7 +112,6 @@ function render(time) {
 }
 
     requestAnimationFrame(render);
-    event.target.appendChild(canvas);
 }
 
 function createPoint(radius, widthsegm, heightsegm, colr, emi, xp, yp, zp)
@@ -148,9 +123,6 @@ function createPoint(radius, widthsegm, heightsegm, colr, emi, xp, yp, zp)
     poin.position.x = xp;
     poin.position.y = yp;
     poin.position.z = zp;
-    poin.castShadow = true;
-    poin.receiveShadow = true;
-    //poin.raycast();
     return poin;
 }
 
