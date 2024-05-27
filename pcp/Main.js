8b71e6946b7b0f5f9232e96d4b19f1866efba3b7
Pierre <pierre.wiele@student.htw-berlin.de>
2024-05-27 16:31:48 +0200

    html class -> .not_accessible added and messages created... dialog a bit fixed

--- pcp/Main.js
+++ pcp/Main.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import RenderFileOnCanvas from "./src/Rendering";
 
 var DialogLine = 1;
+const MaxWindows = 8;
 function setup() {
     document.getElementById("Addbtn").addEventListener("click", AddView);
-    //document.getElementById("Removebtn").addEventListener("click", RemoveView);
-    document.getElementById("SetPointsbtn").addEventListener("click", OpenSetPointsMenu);
+    document.getElementById("combine").addEventListener("click", Combine);
+
+    document.getElementById("saveBtn").addEventListener("click", SaveFile);
+    document.getElementById("DarkLightBtn").addEventListener("click", DarkLightMode);
 
     document.getElementById("showOrHideDialog").addEventListener("click", function () {
         if (document.querySelectorAll('#Dialog.minimized').length > 0) {
@@ -17,10 +20,11 @@ function setup() {
         }
     });
     //document.querySelector('#objViewCont .objViewWin canvas').addEventListener("click", event => { RenderOnCanvas(event) });
-    AssignBtns();
-    AddView();
+
     AddToDialog("Good morning folks... ");
     AddToDialog("I don't care for the actual time... Have Fun");
+    AssignBtns();
+    AddView();
 
     // Create a new instance of MutationObserver, passing it a callback function // same as adventlistener, but can handle the change of innerHTML
     const observer = new MutationObserver(function () {
@@ -30,7 +34,18 @@ function setup() {
     });
     // Call 'observe' on the MutationObserver instance, specifying the element to observe
     observer.observe(document.querySelector('#Dialog p'), { childList: true });
-
+}
+function DarkLightMode()
+{
+    AddToDialog("Not implemented yet,...sry ://")
+}
+function SaveFile()
+{
+    AddToDialog("Uuuuhm... Nothing there to save...")
+}
+function Combine()
+{
+    AddToDialog("Sorry, this is not coded yet...")
 }
 function Highlight(element)
 {
@@ -41,32 +56,22 @@ function AssignBtns()
 {
     document.querySelector('.objViewWin:last-child .option-menu .closeBtn').addEventListener("click", event => { RemoveView(event) });
     document.querySelector('.objViewWin:last-child .option-menu .import').addEventListener("change", event => { ImportFile(event.target) });
-    AddToDialog("Advents were just assigned, to two new buttons!)")
-    //document.querySelector('.objViewWin:last-child .option-menu .pointsize').addEventListener("change", event => { ImportFile(event.target.parentNode.querySelector('.import')) });
-    
 }
 function AddView() {
     var viewcont = document.getElementById("objViewCont"); // gets the Element that contains all Viewports
-    if (viewcont.childElementCount < 8) {
+    if (viewcont.childElementCount < MaxWindows) {
 
         viewcont.appendChild(document.querySelector('#objViewCont .objViewWin').cloneNode(true));
-        //viewcont.lastChild.addEventListener("click", event => { RenderFileOnCanvas(event) });
-        //viewcont.lastChild.querySelector('[name="choosefile"]').files[0] = null; 
         AssignBtns();
         if (document.querySelector('.objViewWin:last-child .hint.hidden')) {
             document.getElementsByClassName("hint hidden").item(document.getElementsByClassName("hint hidden").length - 1).classList.remove("hidden");
         }
-
-        var combBtn = document.getElementById("combine"); // from here the appearance/location of the combine Btn is decided
-        if (viewcont.childElementCount == 2) {
-            combBtn.classList.remove("hidden");
-        }
-        else if (viewcont.childElementCount == 3) {
-            combBtn.style.bottom = "3%";
-            combBtn.style.right = "calc(3% + 18em)";
-            combBtn.style.top = "unset";
-        }  
-    }    
+        if (viewcont.childElementCount == MaxWindows) { document.getElementById('Addbtn').classList.add("not_accessible"); }
+    }
+    else
+    {
+        AddToDialog("Sorry, you can't have more than " + MaxWindows + " Windows open");
+    }
 }
 function ImportFile(eventtarget)
 {
@@ -82,13 +87,15 @@ function AddToDialog(diamessage)
     document.querySelector('#Dialog p').innerHTML += "<br>" + DialogLine + ": " + diamessage;
     DialogLine++;
 }
-function RemoveView(event) {
-    
+function RemoveView(event)
+{    
     var viewCont = document.getElementById("objViewCont");
-    if (viewCont.childElementCount > 1) {
+    if (viewCont.childElementCount > 1)
+    {
         viewCont.removeChild(event.target.parentNode.parentNode);
-        if (viewCont.childElementCount == 1) {
-            document.getElementById("combine").classList.add("hidden");
+        if (viewCont.childElementCount == (MaxWindows - 1))
+        {
+            document.getElementById('Addbtn').classList.remove("not_accessible");
         }
     }
 }
